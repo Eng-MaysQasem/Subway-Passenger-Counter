6d298058230f7e9f128a089822b7d8014a6fc288
Eng-MaysQasem <mbqasem7@gmail.com>
2025-05-12 19:44:20 +0300

    Store The Num Of Passengers And Rest Counter

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 let CountElm = document.getElementById('counter');
+let SaveElm = document.getElementById('save-el');
 let counter = 0;
+
 function CalculatePassenger() {
     counter += 1;
     CountElm.innerText = counter;
+}
 
+function Save() {
+    let countStr = counter + " - ";
+    SaveElm.textContent += countStr;
+    CountElm.innerText = 0;
+    counter = 0;
 }
